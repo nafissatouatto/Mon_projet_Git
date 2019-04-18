27f76758db7e8ec06d54a8415cb8fd31fad45cb3
nafissa <dian_83@live.fr>
2019-04-18 16:48:19 +0200

    Ajout commit

--- src/com/java/formation/Main.java
+++ src/com/java/formation/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Bonjour à tous ceci est mon premier projet que je partage sur GitHub");
+	System.out.println("Ajout d'un commit");
     }
 }

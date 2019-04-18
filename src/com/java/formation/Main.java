319fa76d8cb5c1ea6ddb0e427fecd10e74df15a9
nafissa <dian_83@live.fr>
2019-04-18 16:45:38 +0200

    Initial commit

---
+++ src/com/java/formation/Main.java
@@ -0,0 +1,8 @@
+package com.java.formation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Bonjour à tous ceci est mon premier projet que je partage sur GitHub");
+    }
+}
